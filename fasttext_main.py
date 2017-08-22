f243394c41cb63f1a60a449a9eff797cffaeea3f
nur <nur@sofitlabs.ru>
2017-08-22 08:03:05 +0300

    add fasttext

---
+++ fasttext_main.py
@@ -0,0 +1,62 @@
+import fasttext
+from pathes import path_to_data
+from util_funcs import read_texts, read_labels
+import re
+
+def update_text_for_fasttext():
+    texts = read_texts()
+
+    for i in range(len(texts)):
+        texts[i] =  re.sub("[^a-zA-Z]+", "", texts[i])
+        print (texts[i])
+
+    exit()
+
+    labels = read_labels()
+
+    num_sep = len(texts) / 100 * 80
+    num_sep = int(num_sep)
+
+
+    num = 0
+    with open(path_to_data + "texts_updated_train", "w") as f:
+        for i in texts:
+            f.write("__label__" + labels[num] + " " + i.lower() + "\n")
+            num += 1
+
+            if num_sep == num:
+                break
+
+    num = 0
+    with open(path_to_data + "texts_updated_test", "w") as f:
+        for i in texts:
+            if num > num_sep:
+                f.write("__label__" + labels[num] + " " + i.lower() + "\n")
+
+            num += 1
+
+            #if num_sep == num:
+            #    break
+
+    #exit()
+update_text_for_fasttext()
+
+#print (dir(fasttext))
+#exit()
+# Skipgram model
+#model = fasttext.skipgram(path_to_data + "texts", 'model')
+
+classifier = fasttext.supervised(path_to_data + "texts_updated_test", 'model', label_prefix='__label__')
+
+result = classifier.test(path_to_data + "texts_updated_test")
+print ('P@1:', result.precision)
+print ('R@1:', result.recall)
+print ('Number of examples:', result.nexamples)
+
+# >> cat cooking.stackexchange.txt | sed -e "s/([.!?,'/()])/ 1 /g" | tr "[:upper:]" "[:lower:]" > cooking.preprocessed.txt
+
+#print (model.words) # list of words in dictionary
+
+# CBOW model
+#model = fasttext.cbow('data.txt', 'model')
+#print model.words # list of words in dictionary
